ac7baf0c00773f5b543bfb9905e7e93420ce49d6
bxf1001 <149566631+bxf1001@users.noreply.github.com>
2024-03-10 17:13:48 +0530

    Update MainWindow.py

--- MainWindow.py
+++ MainWindow.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
 from qt_material import apply_stylesheet
 from Add_user import AnotherWindow
-from retrive_data import UserDataWidget
+
 
 class MyMainWindow(QMainWindow):
     def __init__(self):
@@ -11,7 +11,7 @@ class MyMainWindow(QMainWindow):
         self.setGeometry(100, 100, 300, 300)  # Set window size
 
         # Apply the Qt Material theme (choose a theme from the available options)
-        apply_stylesheet(self, theme='dark_teal.xml')
+        
 
         # Create buttons
         button_add_user = QPushButton("Add User", self)
@@ -33,7 +33,7 @@ class MyMainWindow(QMainWindow):
         button_exit.clicked.connect(self.close)  # Close the application when clicked
 
         self.new_window = AnotherWindow() # Create an instance of the new window class
-        self.new_window_1 = UserDataWidget()
+
     def open_new_window(self):
         # This method is the slot that will show the new window when the add user button is clicked
         self.new_window.show() # Show the new window
@@ -49,5 +49,6 @@ class MyMainWindow(QMainWindow):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyMainWindow()
+    apply_stylesheet(app, theme='dark_teal.xml', extra={'round': True})
     window.show()
     sys.exit(app.exec_())
