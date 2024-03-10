92b96068caea39a125eab2f49ebc25e377dd8ae3
bxf1001 <149566631+bxf1001@users.noreply.github.com>
2024-03-10 17:18:05 +0530

    Update MainWindow.py

--- MainWindow.py
+++ MainWindow.py
@@ -1,18 +1,18 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
 from qt_material import apply_stylesheet
-from Add_user import AnotherWindow
+from Add_user import AddUser
 
 
 class MyMainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("My App")
+        self.setWindowTitle("Phone Portal V4")
         self.setGeometry(100, 100, 300, 300)  # Set window size
 
         # Apply the Qt Material theme (choose a theme from the available options)
-        
-
+        apply_stylesheet(self, theme='dark_teal.xml')
+        self.adduser = AddUser()
         # Create buttons
         button_add_user = QPushButton("Add User", self)
         button_add_user.setGeometry(50, 50, 200, 40)
@@ -32,11 +32,10 @@ class MyMainWindow(QMainWindow):
         button_exit.setGeometry(50, 250, 200, 40)
         button_exit.clicked.connect(self.close)  # Close the application when clicked
 
-        self.new_window = AnotherWindow() # Create an instance of the new window class
 
     def open_new_window(self):
         # This method is the slot that will show the new window when the add user button is clicked
-        self.new_window.show() # Show the new window
+        self.adduser.show() # Show the new window
         # self.hide() # Optionally, you can hide the main window
         # self.close() # Optionally, you can close the main window
     def open_new_window_1(self):
@@ -49,6 +48,5 @@ class MyMainWindow(QMainWindow):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyMainWindow()
-    apply_stylesheet(app, theme='dark_teal.xml', extra={'round': True})
     window.show()
     sys.exit(app.exec_())
